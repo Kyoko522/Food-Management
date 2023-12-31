6b3f67e08006357ddec5daf8eb5f104935d3f231
Sheel Patel <sheelpatel5252@gmail.com>
2023-12-30 20:06:40 -0500

    redirecting button on login page works (register -> login)

--- app/src/main/java/com/example/pacsxvirofoodmanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/pacsxvirofoodmanagementapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.pacsxvirofoodmanagementapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val registerButton: Button = findViewById(R.id.to_register_page)
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this, register_page::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/pacsxvirofoodmanagementapp/register_page.kt
+++ app/src/main/java/com/example/pacsxvirofoodmanagementapp/register_page.kt
@@ -1,18 +1,22 @@
 package com.example.pacsxvirofoodmanagementapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.pacsxvirofoodmanagementapp.ui.main.RegisterPageFragment
+import android.widget.Button
 
 class register_page : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, RegisterPageFragment.newInstance())
-                .commitNow()
+
+            val login_button: Button = findViewById(R.id.back_to_login)
+
+            login_button.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
         }
     }
-}
