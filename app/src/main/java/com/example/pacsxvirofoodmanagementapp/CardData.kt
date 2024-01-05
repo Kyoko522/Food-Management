5c0b82a938ac8208692d42923181506853025877
Sheel Patel <115194165+Kyoko522@users.noreply.github.com>
2024-01-05 02:38:20 -0500

    created pop-card layout and removed cards from homepage

---
+++ app/src/main/java/com/example/pacsxvirofoodmanagementapp/CardData.kt
@@ -0,0 +1,20 @@
+package com.example.pacsxvirofoodmanagementapp.ui.main
+
+class CardData {
+    val club_name: String
+    val pickup_location: String
+    val stock_left: Int
+    val description: String
+    val vegan: Boolean
+    val beef: Boolean
+    val vegatarian: Boolean
+    val chicken: Boolean
+    val halal: Boolean
+    val fish: Boolean
+    val dairy_free: Boolean
+    val shellfish: Boolean
+    val glutan_free: Boolean
+    val pork: Boolean
+    val peanut_free: Boolean
+    val other_meat: Boolean
+}
